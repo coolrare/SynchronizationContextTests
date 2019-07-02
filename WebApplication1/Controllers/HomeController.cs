888dfafced47be07c3f55f723ad65e4c508e8d31
Will <doggy.huang@gmail.com>
2019-07-03 00:56:54 +0800

    做更多 SynchronizationContext 與 HttpContext 與 ConfigureAwait 測試

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace WebApplication1.Controllers
@@ -15,33 +11,102 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public async Task<ActionResult> About()
+        public ActionResult Task1(bool continueOnCapturedContext = false)
         {
-            var sc = SynchronizationContext.Current;
-
+            System.Web.HttpContext.Current.Items["data"] = "TEST";
+            ViewBag.SynchronizationContext = SynchronizationContext.Current;
             ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
-
-            await Task.Run(() =>
-            {
-                ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
-                ViewBag.Message = "測試頁面";
-            }).ConfigureAwait(false);
-
-            sc.Post((x) =>
-            {
-                ViewBag.ThreadID3 = Thread.CurrentThread.ManagedThreadId;
-            }, null);
-
-            ViewBag.ThreadID4 = Thread.CurrentThread.ManagedThreadId;
-
+            ViewBag.Data = GetAsync(continueOnCapturedContext).Result;
+            ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
+            ViewBag.HttpContext = System.Web.HttpContext.Current;
             return View();
         }
 
-        public ActionResult Contact()
+        public async Task<ActionResult> Task2(bool continueOnCapturedContext = false)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            System.Web.HttpContext.Current.Items["data"] = "TEST";
+            ViewBag.SynchronizationContext = SynchronizationContext.Current;
+            ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
+            ViewBag.Data = GetAsync(continueOnCapturedContext).Result;
+            ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
+            ViewBag.HttpContext = System.Web.HttpContext.Current;
+            return View("Task1");
         }
+
+        public async Task<ActionResult> Task3(bool continueOnCapturedContext = false)
+        {
+            System.Web.HttpContext.Current.Items["data"] = "TEST";
+            ViewBag.SynchronizationContext = SynchronizationContext.Current;
+            ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
+            // 可透過 SynchronizationContext.Current 的 Send/Post 方法帶 CurrentThread 過去
+            ViewBag.Data = await GetAsync(SynchronizationContext.Current, continueOnCapturedContext);
+            ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
+            return View("Task1");
+        }
+
+        public async Task<ActionResult> Task4(bool continueOnCapturedContext = false)
+        {
+            System.Web.HttpContext.Current.Items["data"] = "TEST";
+            ViewBag.SynchronizationContext = SynchronizationContext.Current;
+            ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
+            // 直接傳送 System.Web.HttpContext.Current 過去可以在不同執行緒中共用該物件
+            ViewBag.Data = await GetAsync(System.Web.HttpContext.Current, continueOnCapturedContext);
+            ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
+            return View("Task1");
+        }
+
+
+        private async Task<string> GetAsync(bool continueOnCapturedContext = false)
+        {
+            return await Task.Run(() =>
+            {
+                // 由於在不同執行緒，所以抓不到 System.Web.HttpContext.Current
+                if (System.Web.HttpContext.Current != null)
+                {
+                    ViewBag.ItemsData = System.Web.HttpContext.Current.Items["data"];
+                }
+
+                Thread.Sleep(1000);
+
+                return $"GetAsync's ThreadID = {Thread.CurrentThread.ManagedThreadId}";
+            })
+            .ConfigureAwait(continueOnCapturedContext);
+        }
+
+
+        private async Task<string> GetAsync(SynchronizationContext sc, bool continueOnCapturedContext = false)
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+
+                // 如果是 WPF/WinForm 的話，就可以使用 sc.Post() 非同步方法
+                sc.Send((x) =>
+                {
+                    // 由於透過「同步內容」所以可以得到 HttpContext.Current 物件
+                    ViewBag.HttpContext = System.Web.HttpContext.Current;
+                    ViewBag.ItemsData = System.Web.HttpContext.Current.Items["data"];
+                }, null);
+
+                return $"GetAsync's ThreadID = {Thread.CurrentThread.ManagedThreadId}";
+
+            })
+            .ConfigureAwait(continueOnCapturedContext);
+        }
+
+        private async Task<string> GetAsync(System.Web.HttpContext context, bool continueOnCapturedContext = false)
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+
+                ViewBag.HttpContext = context;
+                ViewBag.ItemsData = context.Items["data"];
+
+                return $"GetAsync's ThreadID = {Thread.CurrentThread.ManagedThreadId}";
+            })
+            .ConfigureAwait(continueOnCapturedContext);
+        }
+
     }
 }
