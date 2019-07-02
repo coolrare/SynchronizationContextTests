eae51037cf88d747acda892a4bfe6d3df174a71d
Will <doggy.huang@gmail.com>
2019-07-03 01:12:40 +0800

    加入一個非同步範例，如果「非同步」方法已經處於完成狀態，就算呼叫 Wait() 或 Result 也不會產生 Deadlock

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -16,6 +16,8 @@ namespace WebApplication1.Controllers
             System.Web.HttpContext.Current.Items["data"] = "TEST";
             ViewBag.SynchronizationContext = SynchronizationContext.Current;
             ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
+            // 不要使用 Blocking Thread 的方式在「非同步」方法上
+            // 如果「非同步」方法尚未結束就呼叫 Wait() 或 Result 就會產生 Deadlock
             ViewBag.Data = GetAsync(continueOnCapturedContext).Result;
             ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
             ViewBag.HttpContext = System.Web.HttpContext.Current;
@@ -27,6 +29,8 @@ namespace WebApplication1.Controllers
             System.Web.HttpContext.Current.Items["data"] = "TEST";
             ViewBag.SynchronizationContext = SynchronizationContext.Current;
             ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
+            // 不要使用 Blocking Thread 的方式在「非同步」的方法上
+            // 如果「非同步」方法尚未結束就呼叫 Wait() 或 Result 就會產生 Deadlock
             ViewBag.Data = GetAsync(continueOnCapturedContext).Result;
             ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
             ViewBag.HttpContext = System.Web.HttpContext.Current;
@@ -38,9 +42,17 @@ namespace WebApplication1.Controllers
             System.Web.HttpContext.Current.Items["data"] = "TEST";
             ViewBag.SynchronizationContext = SynchronizationContext.Current;
             ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
-            // 可透過 SynchronizationContext.Current 的 Send/Post 方法帶 CurrentThread 過去
-            ViewBag.Data = await GetAsync(SynchronizationContext.Current, continueOnCapturedContext);
+
+            Task<string> t = GetAsync(continueOnCapturedContext);
+
+            // 等到非同步方法執行完畢
+            await t;
+
+            // 如果「非同步」方法已經處於完成狀態，就算呼叫 Wait() 或 Result 也不會產生 Deadlock
+            ViewBag.Data = t.Result;
+
             ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
+            ViewBag.HttpContext = System.Web.HttpContext.Current;
             return View("Task1");
         }
 
@@ -49,7 +61,18 @@ namespace WebApplication1.Controllers
             System.Web.HttpContext.Current.Items["data"] = "TEST";
             ViewBag.SynchronizationContext = SynchronizationContext.Current;
             ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
-            // 直接傳送 System.Web.HttpContext.Current 過去可以在不同執行緒中共用該物件
+            // 可透過 SynchronizationContext.Current 的 Send/Post 方法帶 CurrentThread 過去
+            ViewBag.Data = await GetAsync(SynchronizationContext.Current, continueOnCapturedContext);
+            ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
+            return View("Task1");
+        }
+
+        public async Task<ActionResult> Task5(bool continueOnCapturedContext = false)
+        {
+            System.Web.HttpContext.Current.Items["data"] = "TEST";
+            ViewBag.SynchronizationContext = SynchronizationContext.Current;
+            ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
+            // 直接傳送 System.Web.HttpContext.Current 過去可以在不同執行緒中共用該物件 (Thread Safe)
             ViewBag.Data = await GetAsync(System.Web.HttpContext.Current, continueOnCapturedContext);
             ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
             return View("Task1");
