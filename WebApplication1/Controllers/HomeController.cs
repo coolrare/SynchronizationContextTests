a8a988c278c0f8f6d517b4f5c7708d5f7a29c8c0
Will <doggy.huang@gmail.com>
2019-07-02 09:47:56 +0800

    測試 SynchronizationContext 如何使用 Thread

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,9 +15,24 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewBag.Message = "Your application description page.";
+            var sc = SynchronizationContext.Current;
+
+            ViewBag.ThreadID1 = Thread.CurrentThread.ManagedThreadId;
+
+            await Task.Run(() =>
+            {
+                ViewBag.ThreadID2 = Thread.CurrentThread.ManagedThreadId;
+                ViewBag.Message = "測試頁面";
+            }).ConfigureAwait(false);
+
+            sc.Post((x) =>
+            {
+                ViewBag.ThreadID3 = Thread.CurrentThread.ManagedThreadId;
+            }, null);
+
+            ViewBag.ThreadID4 = Thread.CurrentThread.ManagedThreadId;
 
             return View();
         }
